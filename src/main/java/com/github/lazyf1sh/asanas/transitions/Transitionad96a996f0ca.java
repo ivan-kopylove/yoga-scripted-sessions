460884a0405dcb4465fdc204a35e285bcc60b48b
Ivan Kopylov <ivan.kopylove@gmail.com>
2021-08-27 06:54:57 +0300

    extracted one of the asanas to separate class

---
+++ src/main/java/com/github/lazyf1sh/asanas/transitions/Transitionad96a996f0ca.java
@@ -0,0 +1,11 @@
+package com.github.lazyf1sh.asanas.transitions;
+
+import java.io.IOException;
+
+import static com.github.lazyf1sh.Util.readFile;
+
+public class Transitionad96a996f0ca {
+    public String build() throws IOException {
+        return readFile("Transition-ad96a996f0ca.txt");
+    }
+}
