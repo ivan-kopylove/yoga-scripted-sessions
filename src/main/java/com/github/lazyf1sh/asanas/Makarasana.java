8a899cc6d99c8e8cbdd6e7f952b2827fff545c74
Ivan Kopylov <ivan.kopylove@gmail.com>
2021-09-19 16:39:18 +0300

    updated makarasana implementation details

--- src/main/java/com/github/lazyf1sh/asanas/Makarasana.java
+++ src/main/java/com/github/lazyf1sh/asanas/Makarasana.java
@@ -3,8 +3,9 @@ package com.github.lazyf1sh.asanas;
 import com.github.lazyf1sh.util.YogaConfig;
 
 import java.io.IOException;
+import java.nio.file.Paths;
 
-import static com.github.lazyf1sh.sides.TextReplacer.enrichSidePlaceHolder;
+import static com.github.lazyf1sh.util.Util.readAsana;
 import static com.github.lazyf1sh.util.Util.readFile;
 
 public class Makarasana {
@@ -15,10 +16,7 @@ public class Makarasana {
     }
 
     public String build() throws IOException {
-        String makarasana = readFile("asanas/marakasana/makarasana-payload", yogaConfig.getLanguage());
-
-        makarasana = enrichSidePlaceHolder(yogaConfig.getSide(), makarasana);
-
+        String makarasana = readAsana(Paths.get("marakasana/makarasana-payload"), yogaConfig.getLanguage(), yogaConfig.getSide());
 
         if (yogaConfig.isSanscritMeaning()) {
             makarasana = makarasana.replace("{{makarasana-meaning}}", readFile("interpretations/makara", yogaConfig.getLanguage()));
