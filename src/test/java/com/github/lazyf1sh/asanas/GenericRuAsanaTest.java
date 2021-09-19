70f559d9aef4ba8334941b84fe071e064f2b6f99
Ivan Kopylov <ivan.kopylove@gmail.com>
2021-09-19 18:43:10 +0300

    added generic asana test interface with common names as reminder

---
+++ src/test/java/com/github/lazyf1sh/asanas/GenericRuAsanaTest.java
@@ -0,0 +1,14 @@
+package com.github.lazyf1sh.asanas;
+
+import java.io.IOException;
+
+public interface GenericRuAsanaTest {
+
+    void should_be_of_expected_length_ru() throws IOException;
+
+    void should_contain_asana_name_ru() throws IOException;
+
+    void should_contain_asana_meaning_ru() throws IOException;
+
+    void should_pass_generic_check_ru() throws IOException;
+}
