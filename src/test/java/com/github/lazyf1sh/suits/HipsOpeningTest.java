e742b091857062980f1937f554a95f9ce82d5d65
Ivan Kopylov <ivan.kopylove@gmail.com>
2022-05-24 07:58:54 +0300

    updated based on voice recorded feedback

--- src/test/java/com/github/lazyf1sh/suits/HipsOpeningTest.java
+++ src/test/java/com/github/lazyf1sh/suits/HipsOpeningTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 
+import static com.github.lazyf1sh.test.fixtures.YogaConfigTestFixtures.yogaConfigRuWithMeaning;
 import static com.github.lazyf1sh.util.CommonAssertion.containsNoCurlyBrackets;
 import static com.github.lazyf1sh.util.CommonAssertion.containsNoWords;
 import static org.hamcrest.CoreMatchers.containsString;
@@ -15,22 +16,27 @@ public class HipsOpeningTest {
 
     @Test
     public void should_be_of_expected_size() throws IOException {
-        final YogaConfig yogaConfig = new YogaConfig(true, "ru");
-        final HipsOpening hipsOpening = new HipsOpening(yogaConfig);
+        // given
+        final HipsOpening hipsOpening = new HipsOpening(yogaConfigRuWithMeaning());
 
+        // when
         final String result = hipsOpening.buildHipsOpeningSession();
 
+
+        // then
         assertThat(result.length(), equalTo(46416));
 
     }
 
     @Test
     public void should_contain_specific_lines() throws IOException {
-        final YogaConfig yogaConfig = new YogaConfig(true, "ru");
-        final HipsOpening hipsOpening = new HipsOpening(yogaConfig);
+        // given
+        final HipsOpening hipsOpening = new HipsOpening(yogaConfigRuWithMeaning());
 
+        // when
         final String result = hipsOpening.buildHipsOpeningSession();
 
+        // then
         assertThat(result, containsString("Урдхва хаст+асана на носках"));
         containsNoCurlyBrackets(result);
         containsNoWords(result);
