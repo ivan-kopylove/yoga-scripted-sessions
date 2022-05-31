5d170fd339bffcd3931e25e6a8d96424e2a52d53
Ivan Kopylov <ivan.kopylove@gmail.com>
2022-05-31 09:24:31 +0300

    dasjldas

--- src/main/java/com/github/lazyf1sh/util/UncommentAndRun.java
+++ src/main/java/com/github/lazyf1sh/util/UncommentAndRun.java
@@ -20,16 +20,16 @@ public final class UncommentAndRun {
         final ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
         final YogaConfig yogaConfig = objectMapper.readValue(new File("src/main/resources/yoga.config.yml"), YogaConfig.class);
 
-//        final SuryaNamaskar suryaNamaskar = new SuryaNamaskar(yogaConfig);
-        final HipsOpening hipsOpening = new HipsOpening(yogaConfig);
-//        final Bends bends = new Bends(yogaConfig);
+//        final SuryaNamaskar session = new SuryaNamaskar(yogaConfig);
+//        final HipsOpening session = new HipsOpening(yogaConfig);
+        final Bends session = new Bends(yogaConfig);
 
         final StringBuilder result = new StringBuilder();
 
         result.append("Старт.\n");
         result.append("sil <[20000]>\n");
 
-        result.append(hipsOpening.build());
+        result.append(session.build());
 
 
         final String trimmed = trimmer.multipleTrim(result.toString());
