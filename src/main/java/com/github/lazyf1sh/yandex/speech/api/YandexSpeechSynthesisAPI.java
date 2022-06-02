b22bcd82a930b3fa285fbea1eacd5cd1a6836f60
Ivan Kopylov <ivan.kopylove@gmail.com>
2022-06-02 20:20:26 +0300

    updated based on voice recorded feedback

--- src/main/java/com/github/lazyf1sh/yandex/speech/api/YandexSpeechSynthesisAPI.java
+++ src/main/java/com/github/lazyf1sh/yandex/speech/api/YandexSpeechSynthesisAPI.java
@@ -21,7 +21,7 @@ public final class YandexSpeechSynthesisAPI {
             throw new RuntimeException();
         }
 
-        final String token = "t1.9euelZqXkI2KyYqJnJOWx56NyZDJke3rnpWakpaUyY3Lj5uPi42ai5HHmsjl8_dECiRr-e8nA34X_d3z9wQ5IWv57ycDfhf9.EktJyCgN3Rihf3KakZl_PvDPega8GMDMuYQjfqfUB7-Rhkn1Dzu1GSBuvYc9ud82G_KeOYpxDiLCMOrXUJgDCQ";
+        final String token = "t1.9euelZrMxs7KzMfJk8-Lz82TkJaOle3rnpWakpaUyY3Lj5uPi42ai5HHmsjl8_c6URxr-e8WE1Yj_d3z93p_GWv57xYTViP9.C9P-wI5svz0izdVoqkY3bHtvbFoKX4zUTn5yBNCl1Ulpys9vPj6XtXLiqwcvfFRBrCNsGASUGHECuk0ZWI8_Dg";
         final String folderId = "b1g0vt1m6o1bapc66idu";
 
         final Client client = ClientBuilder.newClient();
