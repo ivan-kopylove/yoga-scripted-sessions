8f8d219f85b5dfe33364960d68b89ff537e67cf1
Ivan Kopylov <ivan.kopylove@gmail.com>
2022-10-19 16:31:02 +0300

    refactor

--- src/test/java/com/github/lazyf1sh/nodes/ekaPadaKaundiniAsanaSecondRightLeg/EkaPadaKaundiniAsanaSecondTest.java
+++ src/test/java/com/github/lazyf1sh/nodes/ekaPadaKaundiniAsanaSecondRightLeg/EkaPadaKaundiniAsanaSecondTest.java
@@ -6,16 +6,27 @@ import java.io.IOException;
 
 import static com.github.lazyf1sh.nodes.ekaPadaKaundiniAsanaSecondRightLeg.EkaPadaKaundiniAsanaSecond.ekaPadaKaundiniAsanaSecond;
 import static com.github.lazyf1sh.sides.Side.LEFT_DEFAULT;
+import static com.github.lazyf1sh.sides.Side.RIGHT;
 import static com.github.lazyf1sh.util.CommonAssertion.allCommonChecks;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 
-public class ekaPadaKaundiniAsanaSecondRightLegTest {
+public class EkaPadaKaundiniAsanaSecondTest {
 
     @Test
-    public void test() throws IOException {
+    public void test_left() throws IOException {
         // when
         String result = ekaPadaKaundiniAsanaSecond(LEFT_DEFAULT);
+
+        // then
+        assertThat(result.length(), equalTo(484));
+        allCommonChecks(result, EkaPadaKaundiniAsanaSecond.class);
+    }
+
+    @Test
+    public void test_right() throws IOException {
+        // when
+        String result = ekaPadaKaundiniAsanaSecond(RIGHT);
 
         // then
         assertThat(result.length(), equalTo(484));
