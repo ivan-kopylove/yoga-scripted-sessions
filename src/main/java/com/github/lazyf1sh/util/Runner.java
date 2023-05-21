4110fd9a2be4126335df4ef12202334b74298364
Ivan Kopylov <ivan.kopylove@gmail.com>
2023-05-21 09:25:13 +0300

    updated based on voice recorded feedback

--- src/main/java/com/github/lazyf1sh/util/Runner.java
+++ src/main/java/com/github/lazyf1sh/util/Runner.java
@@ -3,11 +3,11 @@ package com.github.lazyf1sh.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import com.github.lazyf1sh.suits.SuryaNamaskar;
 
 import java.io.IOException;
 import java.util.List;
 
+import static com.github.lazyf1sh.suits.Bends.bends;
 import static com.github.lazyf1sh.util.ToFileSaver.save;
 
 public final class Runner {
@@ -19,9 +19,9 @@ public final class Runner {
     public static void main(final String[] args) throws IOException {
         final ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
 
-        final SuryaNamaskar session = new SuryaNamaskar();
+//        final SuryaNamaskar session = new SuryaNamaskar();
 //        final HipsOpening session = new HipsOpening();
-//        final String session = bends();
+        final String session = bends();
 
         final StringBuilder result = new StringBuilder();
 
@@ -29,7 +29,7 @@ public final class Runner {
         result.append("Старт.\n");
         result.append("sil<[40000]>\n");
 
-        result.append(session.build());
+        result.append(session);
 
 
         final String distributedPauses = PAUSE_CONVERTER.distributePause(result.toString());
