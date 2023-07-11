03529447f49ecdb4891baac58a5956f7465357ca
Ivan Kopylov <ivan.kopylove@gmail.com>
2023-07-11 19:54:32 +0300

    WIP

--- src/main/java/com/github/lazyf1sh/util/StreamGobbler.java
+++ src/main/java/com/github/lazyf1sh/util/StreamGobbler.java
@@ -8,12 +8,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-import static java.util.function.Function.identity;
-
 public class StreamGobbler implements Runnable {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(StreamGobbler.class);
     private final InputStream inputStream;
     private final String name;
+    private int lines = 0;
 
     public StreamGobbler(InputStream inputStream, String name) {
         this.inputStream = inputStream;
@@ -27,10 +27,14 @@ public class StreamGobbler implements Runnable {
             BufferedReader br = new BufferedReader(isr);
             String line = null;
             while ((line = br.readLine()) != null) {
-                br.lines().forEach(identity()::apply);
+                lines++;
+                if (lines > 30) {
+                    br.lines().forEach(LOGGER::info);
+                    LOGGER.info("lines {}", lines);
+                }
             }
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
+        } catch (IOException e) {
+            LOGGER.error("gobblger error", e);
         }
     }
 }
