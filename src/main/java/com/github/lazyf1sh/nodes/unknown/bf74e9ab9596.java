998a839d015d2d6f8ef16c656c13b1f10792aa3b
Ivan Kopylov <ivan.kopylove@gmail.com>
2023-07-12 23:45:24 +0300

    minor refactor

---
+++ src/main/java/com/github/lazyf1sh/nodes/unknown/bf74e9ab9596.java
@@ -0,0 +1,20 @@
+package com.github.lazyf1sh.nodes.unknown;
+
+import com.github.lazyf1sh.domain.SourceFile;
+import com.github.lazyf1sh.nodes.Asana;
+
+import java.io.IOException;
+
+public class bf74e9ab9596 extends Asana
+{
+
+    private bf74e9ab9596()
+    {
+
+    }
+
+    public static SourceFile bf74e9ab9596() throws IOException
+    {
+        return new bf74e9ab9596().build();
+    }
+}
