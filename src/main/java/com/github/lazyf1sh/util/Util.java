cd95638372e37ff29bcc557b291da6a58710207e
Ivan Kopylov <ivan.kopylove@gmail.com>
2023-07-23 14:48:15 +0300

    github flow fix attempt

--- src/main/java/com/github/lazyf1sh/util/Util.java
+++ src/main/java/com/github/lazyf1sh/util/Util.java
@@ -7,9 +7,7 @@ import com.github.lazyf1sh.domain.SourceFile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -36,34 +34,20 @@ public final class Util
                                    .replace(".", "/")) + "_ru.txt";
 
 
+        final String name2 = params.getResourceBundleClass()
+                                   .getSimpleName();
+
+        URL resource2 = params.getResourceBundleClass()
+                              .getResource(name2 + "_ru.txt");
+
+
         LOGGER.info("Loading resource {}", name);
         URL resource = params.getResourceBundleClass()
                              .getClassLoader()
                              .getResource(name);
-        FileResourcesUtils app = new FileResourcesUtils();
-
-        // read all files from a resources folder
-        try
-        {
-
-            // files from src/main/resources/json
-            List<File> result = app.getAllFilesFromResource("",
-                                                            params.getResourceBundleClass()
-                                                                  .getClassLoader());
-            for (File file : result)
-            {
-                System.out.println("file --- : " + file);
-            }
-        }
-        catch (URISyntaxException | IOException e)
-        {
-            e.printStackTrace();
-        }
 
         Objects.requireNonNull(resource);
         String path = resource.getPath();
-
-        path = path.replaceFirst("/", "");
 
         final Path path1 = Paths.get(path);
 
