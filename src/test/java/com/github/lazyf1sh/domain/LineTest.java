7d97e0910b3378b43b74657efa340d8b77f9a879
Ivan Kopylov <ivan.kopylove@gmail.com>
2023-08-14 15:02:41 +0300

    updated based on voice recorded feedback

---
+++ src/test/java/com/github/lazyf1sh/domain/LineTest.java
@@ -0,0 +1,23 @@
+package com.github.lazyf1sh.domain;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class LineTest
+{
+    @Test
+    public void run() throws JsonProcessingException
+    {
+        String line = """
+                {"ru":"Поза ребенка.", "chance": 98.1}
+                """;
+
+
+        Line lineObj = new Line(line);
+
+        assertThat(lineObj.chance(), equalTo(98.1));
+    }
+}
