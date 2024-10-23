0cc636aabefd25cc42d8c8143448e99f2bd49ae4
Ivan Kopylov <ivan.kopylove@gmail.com>
2024-10-23 20:41:18 +0300

    wip

--- src/main/java/com/github/lazyf1sh/logic/Runner.java
+++ src/main/java/com/github/lazyf1sh/logic/Runner.java
@@ -27,6 +27,7 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 public final class Runner {
     private static final Logger LOGGER = LoggerFactory.getLogger(Runner.class);
 
+
     private Runner() {
     }
 
@@ -36,12 +37,13 @@ public final class Runner {
         String ycApiFolderId = System.getenv(YC_API_FOLDER_ID.name());
         String serviceAccountId = System.getenv(YANDEX_CLOUD_SERVICE_ACCOUNT_ID.name());
         String keyId = System.getenv(YANDEX_CLOUD_AUTHORIZED_KEY_ID.name());
-        String yandexCloudIamTokenSource = "https://iam.api.cloud.yandex.net/iam/v1/tokens";
-        Path of = Path.of(System.getProperty("user.home"), "yandex-api-authorized.private_key");
+
+
+        Path of = Path.of(System.getProperty("user.home"), YC_API_AUTHORIZED_KEY);
 
 
         JWTTokenBuilder jwtTokenBuilder = new JWTTokenBuilder();
-        String encodedToken = jwtTokenBuilder.buildJwtToken(serviceAccountId, keyId, of, yandexCloudIamTokenSource);
+        String encodedToken = jwtTokenBuilder.buildJwtToken(serviceAccountId, keyId, of, YC_IAM_TOKEN_SOURCE);
 
         YandexApiJwtClient yandexApiJwtClient = new YandexApiJwtClient();
         String iamToken = yandexApiJwtClient.requestIamToken(encodedToken);
@@ -65,22 +67,14 @@ public final class Runner {
         }
 
         createDirectories(Paths.get(CACHE));
-        Path dir = Paths.get(sessionParameters.session()
-                .getSimpleName() + "_" + now().toString()
-                .replace(":", "_"));
+        Path dir = Paths.get(sessionParameters.session().getSimpleName() + "_" + now().toString().replace(":", "_"));
 
 
         sessionParameters.workingDir(dir);
 
         ShellExecutor shellExecutor = new ShellExecutor(sessionParameters);
 
-        Processor processor = new Processor(sessionParameters,
-                new ToFileSaver(sessionParameters,
-                        shellExecutor,
-                        new VoiceProvider(new YandexSpeechSynthesisAPI(
-                                sessionParameters), new Cache(sessionParameters))),
-                shellExecutor,
-                new Translator());
+        Processor processor = new Processor(sessionParameters, new ToFileSaver(sessionParameters, shellExecutor, new VoiceProvider(new YandexSpeechSynthesisAPI(sessionParameters), new Cache(sessionParameters))), shellExecutor, new Translator());
 
         LOGGER.info("executing processor");
         processor.process();
@@ -95,12 +89,7 @@ public final class Runner {
         LOGGER.info("Yandex API retries: {}", sessionParameters.getYandexApiRetries());
         LOGGER.info("Skipped by chance: {}", sessionParameters.getSkippedByChance());
 
-        LOGGER.info("total: {} | ru: {} ({}%) | en: {} ({}%)",
-                sessionParameters.getTotalLines(),
-                sessionParameters.getRuLines(),
-                (int) (sessionParameters.getRuLines() / (double) sessionParameters.getTotalLines() * 100),
-                sessionParameters.getEnLines(),
-                (int) (sessionParameters.getEnLines() / (double) sessionParameters.getTotalLines() * 100));
+        LOGGER.info("total: {} | ru: {} ({}%) | en: {} ({}%)", sessionParameters.getTotalLines(), sessionParameters.getRuLines(), (int) (sessionParameters.getRuLines() / (double) sessionParameters.getTotalLines() * 100), sessionParameters.getEnLines(), (int) (sessionParameters.getEnLines() / (double) sessionParameters.getTotalLines() * 100));
     }
 
     private static void shutDownGobblerExecutor(SessionParameters sessionParameters) {
--- src/main/java/com/github/lazyf1sh/logic/YandexApiEnvironmentVariable.java
+++ src/main/java/com/github/lazyf1sh/logic/YandexApiEnvironmentVariable.java
@@ -4,5 +4,11 @@ package com.github.lazyf1sh.logic;
  * All these items are available in yandex console, no need to add screenshots and deep instructions - the same names in yandex console
  */
 public enum YandexApiEnvironmentVariable {
-    YC_API_FOLDER_ID, YANDEX_CLOUD_SERVICE_ACCOUNT_ID, YANDEX_CLOUD_AUTHORIZED_KEY_ID,
+    YC_API_FOLDER_ID, YANDEX_CLOUD_SERVICE_ACCOUNT_ID, YANDEX_CLOUD_AUTHORIZED_KEY_ID;
+
+    /**
+     * Last time it was refered the same way in yandex cloud console - 'an authorized key'
+     */
+    public static String YC_API_AUTHORIZED_KEY = "yandex-api-authorized.private_key";
+    public static String YC_IAM_TOKEN_SOURCE = "https://iam.api.cloud.yandex.net/iam/v1/tokens";
 }
