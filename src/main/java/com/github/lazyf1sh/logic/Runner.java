71604beab6cafbda6a413553c360168b12868b81
Ivan Kopylov <ivan.kopylove@gmail.com>
2024-10-29 09:12:29 +0300

    wip

--- src/main/java/com/github/lazyf1sh/logic/Runner.java
+++ src/main/java/com/github/lazyf1sh/logic/Runner.java
@@ -3,6 +3,7 @@ package com.github.lazyf1sh.logic;
 import com.github.lazyf1sh.api.YandexApiJwtClient;
 import com.github.lazyf1sh.api.deeplx.DeepLXClient;
 import com.github.lazyf1sh.api.yandex.YandexSpeechSynthesisAPI;
+import com.github.lazyf1sh.asanas.named.Bends;
 import com.github.lazyf1sh.asanas.named.SuryaNamaskar;
 import com.github.lazyf1sh.asanas.named.hipsOpening.HipsOpening;
 import com.github.lazyf1sh.domain.SessionParameters;
