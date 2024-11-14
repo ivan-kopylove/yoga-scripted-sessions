ec7bbb56c0d1c4790afb571f9f9bce661dab5988
Ivan Kopylov <ivan.kopylove@gmail.com>
2024-11-14 19:59:42 +0300

    wip

--- src/test/java/com/github/lazyf1sh/domain/LineTest.java
+++ src/test/java/com/github/lazyf1sh/domain/LineTest.java
@@ -12,7 +12,7 @@ public class LineTest
     public void run() throws JsonProcessingException
     {
         String line = """
-                { "ru": "Поза ребенка.", "chance": 89 }
+                { "ru": "Поза ребенка.", "chance": 88 }
                 """;
 
 
