cc9bd26a9cb17266febf12d1d859bda9ff13bb46
Ivan Kopylov <ivan.kopylove@gmail.com>
2024-12-22 16:38:38 +0300

    wip

--- src/main/java/com/github/lazyf1sh/logic/Contants.java
+++ src/main/java/com/github/lazyf1sh/logic/Contants.java
@@ -3,7 +3,6 @@ package com.github.lazyf1sh.logic;
 public class Contants
 {
     public static final String SIL             = "sil<[";
-    public static final String CLOSING_BRACKET = "]>";
 
     private Contants() {}
 }
