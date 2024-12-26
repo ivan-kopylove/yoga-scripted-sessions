8d6e30bb56da669d6c92ffcdafc85fff5511ce13
Ivan Kopylov <ivan.kopylove@gmail.com>
2024-12-26 20:41:22 +0300

    wip

--- src/main/java/com/github/lazyf1sh/logic/serialization/adapter/SerializeToObjectAdapter.java
+++ src/main/java/com/github/lazyf1sh/logic/serialization/adapter/SerializeToObjectAdapter.java
@@ -2,11 +2,16 @@ package com.github.lazyf1sh.logic.serialization.adapter;
 
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.github.lazyf1sh.logic.Cache;
 import com.github.lazyf1sh.logic.serialization.spi.SerializeToObjectSpi;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static com.github.ivan.kopylove.commons.serialization.CommonSerializationConfig.getCommonSerializer;
 
 public class SerializeToObjectAdapter implements SerializeToObjectSpi {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Cache.class);
 
     @Override
     public <T> T deserialize(Payload<T> myPayload) {
@@ -14,6 +19,7 @@ public class SerializeToObjectAdapter implements SerializeToObjectSpi {
         try {
             return getCommonSerializer().readValue(myPayload.json(), myPayload.type());
         } catch (JsonProcessingException e) {
+            LOGGER.trace(e.getLocalizedMessage(), e);
             throw new RuntimeException(e);
         }
     }
