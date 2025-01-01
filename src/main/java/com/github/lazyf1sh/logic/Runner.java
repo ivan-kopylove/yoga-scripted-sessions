9ab3048ae4a6daa29d733da0226bfc4c2b266f6a
Ivan Kopylov <ivan.kopylove@gmail.com>
2025-01-01 18:07:13 +0300

    wip

--- src/main/java/com/github/lazyf1sh/logic/Runner.java
+++ src/main/java/com/github/lazyf1sh/logic/Runner.java
@@ -7,6 +7,7 @@ import com.github.ivan.kopylove.commons.client.yandex.api.speech.YandexApiParame
 import com.github.ivan.kopylove.commons.client.yandex.api.speech.YandexSpeechSynthesisAPI;
 import com.github.ivan.kopylove.commons.util.JWTTokenBuilder;
 import com.github.lazyf1sh.asanas.named.*;
+import com.github.lazyf1sh.asanas.named.hipsOpening.*;
 import com.github.lazyf1sh.domain.SessionParameters;
 import com.github.lazyf1sh.logic.phrase.common.adapter.CommonBeginningConfigurationExecutorAdapter;
 import com.github.lazyf1sh.logic.phrase.common.usecase.CommonBeginningConfigurationExecutorUseCase;
@@ -62,7 +63,7 @@ public final class Runner {
         String iamToken = yandexApiJwtClient.requestIamToken(encodedToken);
 
         SessionParameters sessionParameters = new SessionParameters();
-        sessionParameters.session(Bends.class);
+        sessionParameters.session(HipsOpening.class);
         createDirectories(Paths.get(CACHE));
         Path dir = Paths.get(sessionParameters.session().getSimpleName() + "_" + now().toString().replace(":", "_"));
         sessionParameters.workingDir(dir);
